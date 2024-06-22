09f759527215e0eed4840b25fcc200e25b12d4a1
Martin Reyes <martin.reyes@unmsm.edu.pe>
2024-06-22 15:54:06 -0500

    Implementado el chatbot.

---
+++ apps/ia/views.py
@@ -0,0 +1,33 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from rest_framework import status
+
+from .models import ChatBot
+from .serializers import ChatBotSerializer
+
+import google.generativeai as genai
+
+# Create a generative model
+genai.configure(api_key= 'AIzaSyD-QCHV_3TeGPknu_S2-SnjBfrIIUA5tfg')
+
+# Create your views here.
+@api_view(['GET'])
+def chatbot(request):
+    data = request.data
+    
+    text = data['text_input']
+    model = genai.GenerativeModel('gemini-pro')
+    chat = model.start_chat()
+    response = chat.send_message(text)
+    
+    print(response.text)
+
+    chatbot = ChatBot.objects.create(
+        text_input= text,
+        gemini_output= response.text
+    )
+
+    serializer = ChatBotSerializer(chatbot)
+    
+    return Response(serializer.data)
--- backend/settings.py
+++ backend/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from dotenv import load_dotenv
+import os
 from pathlib import Path
+
+# Cargar variables de entorno.
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,26 +24,42 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zi+zka$3_h*m21(zm-e$+u-bt(gjn(1aj)_y+^)bk768^+)n+p'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
+# ALLOWED_HOSTS significa que hosts pueden acceder a la aplicación.
 ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
+PROYECT_APPS = [
+    'apps.ia'
+]
+
+THIRD_PARTY_APPS = [
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist'
+]
+
+INSTALLED_APPS = DJANGO_APPS + PROYECT_APPS + THIRD_PARTY_APPS
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -103,12 +123,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'es'
+TIME_ZONE = 'America/Lima'
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
 
@@ -121,3 +139,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
+]
