c467e9e6a003dfa98120e8a4936f0a99caf89c60
Martin Reyes <martin.reyes@unmsm.edu.pe>
2024-06-22 20:00:27 -0500

    Agregado el requeriments.txt

--- apps/ia/views.py
+++ apps/ia/views.py
@@ -8,21 +8,22 @@ from .serializers import ChatBotSerializer
 
 import google.generativeai as genai
 
-# Create a generative model
+# Create a generative model.
 genai.configure(api_key= 'AIzaSyD-QCHV_3TeGPknu_S2-SnjBfrIIUA5tfg')
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat()
 
 # Create your views here.
 @api_view(['POST'])
 def chatbot(request):
     data = request.data
-    
-    text = data['text_input']
-    model = genai.GenerativeModel('gemini-pro')
-    chat = model.start_chat()
+    text = data['text_input'].strip()
+
+    if not text:
+        return Response({'error': 'Por favor introduce un texto'}, status= status.HTTP_400_BAD_REQUEST)
+
     response = chat.send_message(text)
     
-    print(response.text)
-
     chatbot = ChatBot.objects.create(
         text_input= text,
         gemini_output= response.text
@@ -30,4 +31,4 @@ def chatbot(request):
 
     serializer = ChatBotSerializer(chatbot)
     
-    return Response(serializer.data)
+    return Response(serializer.data, status= status.HTTP_201_CREATED)
